7390714e6737b7fa257dff01d6216d9c790887f3
Jessiix <jessicale171@gmail.com>
2021-07-01 13:42:54 +1000

    hi i did some work on my figure, still alot left to do but im more well versed :)

--- data/table2.r
+++ data/table2.r
@@ -1,1 +1,19 @@
+library(tidyverse)
+library(janitor)
+library(dplyr)
+
+data1 <- read.csv("data/Nichols_et_al_data.csv")
+
+data1 <- data1 %>% 
+  filter(include == 0)
+
+view(data1)
+
+data1 <- rename(claimpercent = claim,
+                claimmoney = moneyclaim,
+                CT_practice = completion.time..practice.included.,
+                CT_payments = completion.time..payments.only.,
+                religiosity = relig,
+                religion = Religion) %>% 
+  select(-CT_practice, -CT_payments, -CT, -CT_cheat, -Religion.Text, -affil_cong)
 
