e82bc0874c7cee9df1c3b3e4c1454bd5e0ed52c4
Victor Bian <vkbian@gmail.com>
2021-07-08 14:07:06 +1000

    Made a grouped table for CIs

--- data/figure2.r
+++ data/figure2.r
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(janitor)
 library(gridExtra)
+library(gmodels)
+
 
 data1 <- read.csv("data/Nichols_et_al_data.csv")
 
@@ -20,7 +22,8 @@ dataA <- data1 %>%
   
   mutate(religiosity = abs(religiosity - 5),
          ritual = abs(ritual - 7),              # reverse coding
-         claimpercent = claimpercent * 100) %>%      #turning this into a percentage value
+         claimpercent = claimpercent * 100,     #turning this into a percentage value
+         affil = as.factor(affil_cong)) %>%      
          
   
   select(cond:ritual, -claimmoney, -sex, -age, -Religion.Text, -religion, -starts_with("CT")) %>% # selecting only the columns required
@@ -39,21 +42,22 @@ dataA$cond[dataA$cond==4] <- 3
 dataA$cond <- factor(dataA$cond, levels= c(0,1,2,3),
                 labels = c("Religious", "Secular", "Noise","Control"))
 
-# creating variables so we can filter out specific values for each plot
-dataB <- dataA
-dataC <- dataA
+  
+#Make a new dataframe that contains CI limits for all conditions based on whether participants are religiously affiliated or not
+
+groupA <- dataA %>% 
+  group_by(cond, affil) %>% 
+  summarise(mean = mean(claimpercent, na.rm = TRUE),
+            sd = sd(claimpercent, na.rm = TRUE),
+            n = n()) %>% 
+  drop_na() %>% 
+  mutate(se = sd / sqrt(n),
+         lowerCI = mean - qt(1 - (0.05/2), n - 1) * se,
+         upperCI = mean + qt(1 - (0.05/2), n - 1) * se) %>% 
+  ungroup()
+  
 
 
-# filtering out NA values for each plot 
-
-dataA <- dataA %>% 
-  filter(!is.na(religiosity))
-
-dataB <- dataB %>% 
-  filter(!is.na(ritual))
-
-dataC <- dataC %>% 
-  filter(!is.na(affil))
 
 # plotting the figure
 
@@ -63,11 +67,10 @@ figA <- ggplot(dataA, aes(religiosity, claimpercent, color = cond)) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + #Removes gridlines from plot
   coord_cartesian(ylim = c(0, 50)) + #Sets y limit to 50
   labs(x = "Religiosity", y = "Percentage claimed", title = "Condition*Religiosity") + #axis labels and title
-  theme(plot.title = element_text(hjust = 0.5), legend.position = "none")  #centres title text and removes legend
-  
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "none")   #centres title text and removes legend
 
 
-figB <- ggplot(dataB, aes(ritual, claimpercent, color = cond)) +
+figB <- ggplot(dataA, aes(ritual, claimpercent, color = cond)) +
   geom_smooth(method = "lm", se = FALSE) +
   theme_light() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
@@ -75,12 +78,11 @@ figB <- ggplot(dataB, aes(ritual, claimpercent, color = cond)) +
   labs(x = "Ritual frequency", y = "Percentage claimed", title = "Condition*Ritual frequency") +
   theme(plot.title = element_text(hjust = 0.5), legend.position = "none")
 
-figC <- ggplot(dataC, aes(affil, claimpercent, color = cond)) +
-  stat_smooth(method = "lm", se = FALSE) +
+figC <- ggplot(groupA, aes(affil, mean, color = cond)) +
+  geom_line(aes(cond = cond)) +
+  geom_errorbar(data = groupA, aes(ymin = lowerCI, ymax = upperCI)) +
   theme_light() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  coord_cartesian(ylim = c(0, 50), xlim = c(0, 1)) +
-  scale_x_discrete(limits = c(0, 1)) +
   labs(x = "Religious affiliation", y = "Percentage claimed", title = "Condition*Religious affiliation") +
   theme(plot.title = element_text(hjust = 0.5)) 
 
