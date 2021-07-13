812b8247005e4d54ea105d8e41743e14ea3f7448
Victor Bian <vkbian@gmail.com>
2021-07-13 16:29:01 +1000

    Using position_nudge() to position geoms, and expanded x and y axis so that the geoms start from origin

--- data/fig_1victor.R
+++ data/fig_1victor.R
@@ -18,7 +18,6 @@ data1 <- data1 %>%
          religion = Religion) %>% 
   select(site, claimpercent, cond, id) %>% 
   mutate(claimpercent = claimpercent * 100) %>% 
-
   as_tibble()
   
 
@@ -33,18 +32,26 @@ data1 <- data1 %>%
   mutate(numberOf = (cond == 0) * 100 + (cond == 1 | cond == 2) * 103 + (cond == 3) * 102) %>% 
   mutate(claimpercent2 = claimpercent / numberOf)
 
-fig1_condition_rp <- ggplot(data1, aes(x = cond)) +
+fig1 <- ggplot(data1, aes(x = cond)) +
   geom_col(
-    aes(x = as.numeric(cond)+.5, y = claimpercent2),
+    aes(y = claimpercent2),
     width = .3
   ) +
   ggdist::stat_halfeye(
     aes(y = claimpercent),
     adjust = .5,
-    width = .5,
+    width = .4,
     .width = 0,
-    point_colour = NA
+    point_colour = NA,
+    position = position_nudge(x = 0.17, y = 0)
   ) +
-  coord_flip()
+  coord_flip() +
+  expand_limits(x = 0, y = 0) +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(expand = c(0,0))
 
-plot(fig1_condition_rp)
+
+  
+
+
+plot(fig1)
